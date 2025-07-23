39ca08627cb32867576d05af9edebe92cd71d52b
Darek <dariuszdawidowicz7@gmail.com>
2025-07-23 16:42:49 +0200

    Poprawka: dodano obsługę błędów w create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -1,3 +1,5 @@
 # Tworzenie użytkownika testowego
 New-LocalUser -Name "testuser" -Password (ConvertTo-SecureString "P@ssw0rd123" -AsPlainText -Force) -FullName "Test User" -Description "Użytkownik testowy"
 Add-LocalGroupMember -Group "Administrators" -Member "testuser"
+
+zmiana2
